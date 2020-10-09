72e2753e220efb79c0a7aae0255339bea3c18f85
MOe Chit thu <zmh1271989@gmail.com>
2020-10-09 21:11:38 +0630

    my first project

---
+++ uad.javascript.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>uad</title>
+
+	<script language="JavaScript">
+		<!--
+			function head (){
+				document.write("Hello"):
+			}
+	</script>
+</head>
+<body>
+	<script language
+</body>
+</html>
